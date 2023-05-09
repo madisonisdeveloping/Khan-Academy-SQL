1b62ca5d2447c3b5e12936d68159a7846dc73dfd
madisonisdeveloping <129033421+madisonisdeveloping@users.noreply.github.com>
2023-05-08 22:13:22 -0400

    1_creating_a_table_and_inserting_data.sql

--- 1_creating_a_table_and_inserting_data.sql
+++ 1_creating_a_table_and_inserting_data.sql
@@ -1,11 +1,15 @@
--- Sim Name List:
+/* Create a basic table.
+
+Character Name List:
 -- Hannah (37)
 -- Marcus (18)
 -- Harold (83)
 
-CREATE TABLE sims (id INTEGER PRIMARY KEY, name TEXT, quantity INTEGER);
+Solution*/
 
-INSERT INTO sims VALUES (1, "Hannah", 37);
-INSERT INTO sims VALUES (2, "Marcus", 18);
-INSERT INTO sims VALUES (3, "Harold", 83);
-SELECT * FROM sims;
+CREATE TABLE people (id INTEGER PRIMARY KEY, name TEXT, quantity INTEGER);
+
+INSERT INTO people VALUES (1, "Hannah", 37);
+INSERT INTO people VALUES (2, "Marcus", 18);
+INSERT INTO people VALUES (3, "Harold", 83);
+SELECT * FROM people;
